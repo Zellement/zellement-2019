58a97ff0ee6156c881b537d824e5808a2efc5964
Dan Farrow <dan@zellement.com>
2019-07-01 22:43:29 +0100

    Updated nav button

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -15,24 +15,24 @@ const Navigation = class extends React.Component {
     const menuActive = this.state.showMenu ? 'is-active' : '';
     const burgerActive = this.state.showMenu ? 'is-active' : '';
     return (     
-    <nav className=" md:float-right">
-      <div className={` ${menuActive} hidden md:block`} >
-        <div className="">
+    <nav className="">
+      <div className={`navigation-wrapper navigation-wrapper--${menuActive} absolute top-0 left-0 p-6 w-full h-screen bg-black`} >
         <ul className="">
           <li className="">
-          <AnimatedLink className="" activeClassName="active" to="/">Home</AnimatedLink>
+            <AnimatedLink className="text-white block" activeClassName="active" to="/">Home</AnimatedLink>
+            <AnimatedLink className="text-white block" activeClassName="active" to="/">About</AnimatedLink>
+            <AnimatedLink className="text-white block" activeClassName="active" to="/">Contact</AnimatedLink>
           </li>
-          </ul>
-        </div>
+        </ul>
       </div>
       <div className={`${burgerActive} navigation-button w-12 h-12 block md:hidden fixed top-0 right-0 bg-black z-10 cursor-pointer `} onClick={this.toggleMenu}>
         <div className={`relative w-full h-full bg-cyan:200 z-100 ${burgerActive}`}>
-          <span className="navigation-lines left-0"></span>
-          <span className="navigation-lines left-0 mt-2"></span>
-          <span className="navigation-lines left-0 mt-4"></span>
-          <span className="navigation-lines right-0"></span>
-          <span className="navigation-lines right-0 mt-2"></span>
-          <span className="navigation-lines right-0 mt-4"></span>
+          <span className="navigation-button__lines left-0"></span>
+          <span className="navigation-button__lines left-0 mt-2"></span>
+          <span className="navigation-button__lines left-0 mt-4"></span>
+          <span className="navigation-button__lines right-0"></span>
+          <span className="navigation-button__lines right-0 mt-2"></span>
+          <span className="navigation-button__lines right-0 mt-4"></span>
         </div>
       </div>
     </nav>
