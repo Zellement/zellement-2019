283e01b7de01ff4f641e32081de1946dcc77c4bf
Dan Farrow <dan@zellement.com>
2019-07-02 21:58:53 +0100

    Updating desktop nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -16,16 +16,16 @@ const Navigation = class extends React.Component {
     const burgerActive = this.state.showMenu ? 'is-active' : '';
     return (     
     <nav className="">
-      <div className={`navigation-wrapper navigation-wrapper--${menuActive} fixed top-0 left-0 p-6 w-full h-screen bg-black`} >
+      <div className={`navigation-wrapper navigation-wrapper--${menuActive} fixed transition-fast transform-x--100 top-0 left-0 p-6 w-full h-screen bg-black sm:relative sm:h-auto sm:float-right sm:top-auto sm:transform-x-0 sm:p-3 sm:w-auto`} >
         <ul className="">
-          <li className="">
-            <AnimatedLink className="text-white block" activeClassName="active" to="/">Home</AnimatedLink>
-            <AnimatedLink className="text-white block" activeClassName="active" to="/">About</AnimatedLink>
-            <AnimatedLink className="text-white block" activeClassName="active" to="/">Contact</AnimatedLink>
+          <li className="sm:inline-block">
+            <AnimatedLink className="text-white block sm:inline-block" activeClassName="active" to="/">Home</AnimatedLink>
+            <AnimatedLink className="text-white block sm:inline-block" activeClassName="active" to="/">About</AnimatedLink>
+            <AnimatedLink className="text-white block sm:inline-block" activeClassName="active" to="/">Contact</AnimatedLink>
           </li>
         </ul>
       </div>
-      <div className={`${burgerActive} navigation-button w-12 h-12 block md:hidden fixed top-0 right-0 bg-black z-10 cursor-pointer `} onClick={this.toggleMenu}>
+      <div className={`${burgerActive} navigation-button w-12 h-12 block fixed top-0 right-0 bg-black z-10 cursor-pointer sm:hidden`} onClick={this.toggleMenu}>
         <div className={`relative w-full h-full bg-cyan:200 z-100 ${burgerActive}`}>
           <span className="navigation-button__lines navigation-button__line-1 left-0"></span>
           <span className="navigation-button__lines navigation-button__line-2 left-0 mt-2"></span>
