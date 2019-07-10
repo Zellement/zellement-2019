92eaebeece2554db6d3d060da59efa79ee95f1d7
[Dan Farrow] <dan@zellement.com>
2019-07-10 23:18:15 +0100

    Adding icons for project

--- src/pages/developer.js
+++ src/pages/developer.js
@@ -1,19 +1,33 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import AnimatedLink from "../components/AnimatedLink"
+import { IconContext } from "react-icons";
+import { DiReact, DiSass } from 'react-icons/di';
 
 const HomePage = () => (
   <Layout>
-  <p className="text-md tracking-wider font-color-font"><AnimatedLink to="/">Dan Farrow</AnimatedLink> > Developer</p>
-	<h1 className="text-5xl text-grey-light leading-none mb-8">Developer.</h1>
-	
-	<p className="py-3 text-lg">I have over {new Date().getFullYear() - 2007} years of experience working in a leading digital marketing and web design company based in Nottingham, UK, and I've been lucky to have the chance to work on a multitude of languages, platforms and frameworks.</p>
-	
-	<p className="pb-3 text-md">In recent years, I have worked predominantly with WordPress, but I have been learning new technologies lately to continue to improve my knowledge.</p>
+  	<div className="max-w-3xl">
+	  	<p className="text-md tracking-wider font-color-font"><AnimatedLink to="/">Dan Farrow</AnimatedLink> > Developer</p>
+		<h1 className="text-5xl text-grey-light mb-8">Developer.</h1>
+		<p className="py-3 text-lg">I have over {new Date().getFullYear() - 2007} years of experience working in a leading digital marketing and web design company based in Nottingham, UK, and I've been lucky to have the chance to work on a multitude of languages, platforms and frameworks.</p>
+		<p className="pb-3 text-md">In recent years, I have worked predominantly with WordPress, but I have been learning new technologies lately to continue to improve my knowledge.</p>
+	</div>
 
-	<h3 className="text-2xl mt-5">Sheringham Shantymen</h3>
-	<p><a href="https://www.shantymen.com">www.shantymen.com</a></p>
+		<div className="flex items-stretch w-100 justify-between">
+			<div>
+				<h3 className="text-2xl mt-5">Sheringham Shantymen</h3>
+				<p><a href="https://www.shantymen.com">www.shantymen.com</a></p>
+			</div>
 
+			<div className="align-right">
+				<IconContext.Provider value={{ size: '2em', className: "text-brown-light" }}>
+					<div className="flex pt-8">
+						<DiReact/>
+						<DiSass/>
+					</div>
+				</IconContext.Provider>
+			</div>
+		</div>
   </Layout>
 )
 
