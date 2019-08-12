b71eca52d3d749c981025db6a9b72e30fceddfa0
Dan Farrow <dan@zellement.com>
2019-08-12 21:19:23 +0100

    Updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,16 +9,22 @@ const HomePage = props => (
 	<h1 className="text-6xl text-grey-light leading-none">Hello.</h1>
 	<h2 className="text-3xl text-grey-light leading-none my-8">I'm Dan Farrow.</h2>
 
-        <Img className="shadow-lg" fluid={props.data.meOne.childImageSharp.fluid} />
-	<ul>
-		<li>A <AnimatedLink className="text-white" to="/developer">WordPress developer</AnimatedLink>.</li>
-		<li>A <AnimatedLink className="text-white" to="/developer">front-end developer</AnimatedLink>.</li>
-		<li>An occasional <AnimatedLink className="text-white" to="/designer">designer</AnimatedLink>.</li>
-		<li>An average <AnimatedLink className="text-white" to="/artist">artist</AnimatedLink>.</li>
-		<li>An experienced <AnimatedLink className="text-white" to="/manager">manager</AnimatedLink>.</li>
-		<li>A budding <AnimatedLink className="text-white" to="/musician">musician</AnimatedLink>.</li>
-		<li>And a simple <AnimatedLink className="text-white" to="/hobbyist">hobbyist</AnimatedLink>.</li>
-	</ul>
+  <div className="relative max-w-xl">
+
+    <Img className="shadow-lg max-w-xl" fluid={props.data.meOne.childImageSharp.fluid} />
+  	
+    <ul className="absolute top-3 right-0">
+  		<li className="bg-brown-standard p-2">A <AnimatedLink className="text-white" to="/developer">WordPress developer</AnimatedLink>.</li>
+  		<li className="bg-brown-standard p-2">A <AnimatedLink className="text-white" to="/developer">front-end developer</AnimatedLink>.</li>
+  		<li className="bg-brown-standard p-2">An occasional <AnimatedLink className="text-white" to="/designer">designer</AnimatedLink>.</li>
+  		<li className="bg-brown-standard p-2">An average <AnimatedLink className="text-white" to="/artist">artist</AnimatedLink>.</li>
+  		<li className="bg-brown-standard p-2">An experienced <AnimatedLink className="text-white" to="/manager">manager</AnimatedLink>.</li>
+  		<li className="bg-brown-standard p-2">A budding <AnimatedLink className="text-white" to="/musician">musician</AnimatedLink>.</li>
+  		<li className="bg-brown-standard p-2">And a simple <AnimatedLink className="text-white" to="/hobbyist">hobbyist</AnimatedLink>.</li>
+  	</ul>
+
+  </div>
+
   </Layout>
 )
 
