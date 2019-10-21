9bcbbcce985899f64c9e65840cfc83b5d77a4fd9
Dan Farrow <dan@zellement.com>
2019-10-21 20:01:13 +0100

    Add Ed and remove random order

--- src/pages/recommended.js
+++ src/pages/recommended.js
@@ -33,6 +33,42 @@ const RecommendedPage = () => {
 				<div className="flex flex-col lg:flex-row lg:flex-wrap">
 
 					<RecommendedPeople
+						recPeopleTitle="Ash Barnard"
+						recPeopleURL="https://www.linkedin.com/in/ash-barnard-94ba7b31/"
+						recPeopleDisplayURL="linkedin.com/in/ash-barnard-94ba7b31/"
+						recPeopleClasses = {"order-" +  + Math.floor(Math.random() * 10)}
+						recPeopleJob="Animator"
+						recPeopleDescription="My brother-in-law, known him since I was 15, and we've both had our own career struggles. Proper proud of him to achieve such a key role at such a reputable company such as Rare.">
+					</RecommendedPeople>
+
+					<RecommendedPeople
+						recPeopleTitle="Andrew Bowley"
+						recPeopleURL="https://www.linkedin.com/in/abowley/"
+						recPeopleDisplayURL="linkedin.com/in/abowley/"
+						recPeopleClasses = {"order-" +  + Math.floor(Math.random() * 10)}
+						recPeopleJob="Manager &amp; Leader"
+						recPeopleDescription="Bowley and I have spent our whole Adtrak careers (starting just 2 weeks apart) helping the company grow, and we've shared some tough times together. I've the uptmost respect for where he has ended up, and the work he's put in to it all.">
+					</RecommendedPeople>
+
+					<RecommendedPeople
+						recPeopleTitle="David 'Ed' Edwards"
+						recPeopleURL="https://www.dgedwards.com/"
+						recPeopleDisplayURL="dgedwards.com"
+						recPeopleClasses = {"order-" +  + Math.floor(Math.random() * 10)}
+						recPeopleJob="Designer, Developer, Writer, Musician"
+						recPeopleDescription="A man with an impressive array of skillsets, we met at University and I have had a huge amount of respect with what he does both professionally and personally. He also has an interesting 'edge'.">
+					</RecommendedPeople>
+
+					<RecommendedPeople
+						recPeopleTitle="Ashley Elton"
+						recPeopleURL="https://www.linkedin.com/in/ashley-elton-318252179/"
+						recPeopleDisplayURL="linkedin.com/in/ashley-elton-318252179/"
+						recPeopleClasses = {"order-" +  + Math.floor(Math.random() * 10)}
+						recPeopleJob="Marketing Strategist &amp; Manager"
+						recPeopleDescription="Ash has been at the forefront of Adtrak for years now, and his ability to keep a ship steady is unsurpassed. He will continue to drive forward in his and his department's knowledge, has an excellent head for strategy and an ability to question the status-quo.">
+					</RecommendedPeople>
+
+					<RecommendedPeople
 						recPeopleTitle="Sarah Farrow"
 						recPeopleURL="http://www.theseeingplace.co.uk/"
 						recPeopleDisplayURL="theseeingplace.co.uk"
@@ -51,30 +87,12 @@ const RecommendedPage = () => {
 					</RecommendedPeople>
 
 					<RecommendedPeople
-						recPeopleTitle="Tom Nightingale"
-						recPeopleURL="https://www.linkedin.com/in/tomnightingale/"
-						recPeopleDisplayURL="linkedin.com/in/tomnightingale/"
-						recPeopleClasses = {"order-" +  + Math.floor(Math.random() * 10)}
-						recPeopleJob="Front-end Developer, Designer &amp; Manager"
-						recPeopleDescription="I've worked with Tom for around a decade, and we've been through thick and thin at the company. I've the utmost respect for Tom and his unrivalled work ethic.  He's also a Forest fan.">
-					</RecommendedPeople>
-
-					<RecommendedPeople
 						recPeopleTitle="Ste Greig"
 						recPeopleURL="http://tangledindesign.com/"
 						recPeopleDisplayURL="tangledindesign.com"
 						recPeopleClasses = {"order-" +  + Math.floor(Math.random() * 10)}
 						recPeopleJob="Front-end Developer &amp; Designer"
 						recPeopleDescription="Ste joined Adtrak way-back-when, left to work in New Zealand, returned to the UK and inevitably found his way back 'home'. He's the master of CSS (he wrote his own book!) and just a great guy.">
-					</RecommendedPeople>
-
-					<RecommendedPeople
-						recPeopleTitle="George Wood"
-						recPeopleURL="http://georgewood.me/"
-						recPeopleDisplayURL="georgewood.me"
-						recPeopleClasses = {"order-" +  + Math.floor(Math.random() * 10)}
-						recPeopleJob="UX/UI Designer &amp; Developer"
-						recPeopleDescription="I've watched George grow from a young Web Designer into a fully-fledged Senior UX/UI Designer, and some of the work he produces is just unreal. I envy his design ability.">
 					</RecommendedPeople>
 
 					<RecommendedPeople
@@ -87,39 +105,12 @@ const RecommendedPage = () => {
 					</RecommendedPeople>
 
 					<RecommendedPeople
-						recPeopleTitle="Ashley Elton"
-						recPeopleURL="https://www.linkedin.com/in/ashley-elton-318252179/"
-						recPeopleDisplayURL="linkedin.com/in/ashley-elton-318252179/"
+						recPeopleTitle="Tom Nightingale"
+						recPeopleURL="https://www.linkedin.com/in/tomnightingale/"
+						recPeopleDisplayURL="linkedin.com/in/tomnightingale/"
 						recPeopleClasses = {"order-" +  + Math.floor(Math.random() * 10)}
-						recPeopleJob="Marketing Strategist &amp; Manager"
-						recPeopleDescription="Ash has been at the forefront of Adtrak for years now, and his ability to keep a ship steady is unsurpassed. He will continue to drive forward in his and his department's knowledge, has an excellent head for strategy and an ability to question the status-quo.">
-					</RecommendedPeople>
-
-					<RecommendedPeople
-						recPeopleTitle="Andrew Bowley"
-						recPeopleURL="https://www.linkedin.com/in/abowley/"
-						recPeopleDisplayURL="linkedin.com/in/abowley/"
-						recPeopleClasses = {"order-" +  + Math.floor(Math.random() * 10)}
-						recPeopleJob="Manager &amp; Leader"
-						recPeopleDescription="Bowley and I have spent our whole Adtrak careers (starting just 2 weeks apart) helping the company grow, and we've shared some tough times together. I've the uptmost respect for where he has ended up, and the work he's put in to it all.">
-					</RecommendedPeople>
-
-					<RecommendedPeople
-						recPeopleTitle="Ben Scothern"
-						recPeopleURL="https://www.linkedin.com/in/ben-scothern-44a04475/"
-						recPeopleDisplayURL="linkedin.com/in/ben-scothern-44a04475/"
-						recPeopleClasses = {"order-" +  + Math.floor(Math.random() * 10)}
-						recPeopleJob="Creative Copywriter"
-						recPeopleDescription="Someone who never fails to make me laugh, Ben's creative ability with words is astounding, was crucial to the re-build of Adtrak's website and has become an important personality across the company.">
-					</RecommendedPeople>
-
-					<RecommendedPeople
-						recPeopleTitle="Ash Barnard"
-						recPeopleURL="https://www.linkedin.com/in/ash-barnard-94ba7b31/"
-						recPeopleDisplayURL="linkedin.com/in/ash-barnard-94ba7b31/"
-						recPeopleClasses = {"order-" +  + Math.floor(Math.random() * 10)}
-						recPeopleJob="Animator"
-						recPeopleDescription="My brother-in-law, known him since I was 15, and we've both had our own career struggles. Proper proud of him to achieve such a key role at such a reputable company such as Rare.">
+						recPeopleJob="Front-end Developer, Designer &amp; Manager"
+						recPeopleDescription="I've worked with Tom for around a decade, and we've been through thick and thin at the company. I've the utmost respect for Tom and his unrivalled work ethic.  He's also a Forest fan.">
 					</RecommendedPeople>
 
 					<RecommendedPeople
@@ -132,6 +123,24 @@ const RecommendedPage = () => {
 					</RecommendedPeople>
 
 					<RecommendedPeople
+						recPeopleTitle="Chris Rutter"
+						recPeopleURL="https://www.linkedin.com/in/iamchrisrutter/"
+						recPeopleDisplayURL="linkedin.com/in/iamchrisrutter/"
+						recPeopleClasses = {"order-" +  + Math.floor(Math.random() * 10)}
+						recPeopleJob="Technical &amp; IT Guru"
+						recPeopleDescription="Chris's technical mind in how to integrate systems is high class - he works very hard in order to make things efficient and will make it his mission to understand a process first in order to improve it.">
+					</RecommendedPeople>
+
+					<RecommendedPeople
+						recPeopleTitle="Ben Scothern"
+						recPeopleURL="https://www.linkedin.com/in/ben-scothern-44a04475/"
+						recPeopleDisplayURL="linkedin.com/in/ben-scothern-44a04475/"
+						recPeopleClasses = {"order-" +  + Math.floor(Math.random() * 10)}
+						recPeopleJob="Creative Copywriter"
+						recPeopleDescription="Someone who never fails to make me laugh, Ben's creative ability with words is astounding, was crucial to the re-build of Adtrak's website and has become an important personality across the company.">
+					</RecommendedPeople>
+
+					<RecommendedPeople
 						recPeopleTitle="Holly Shackleton"
 						recPeopleURL="https://www.linkedin.com/in/holly-shackleton-28b27413a/"
 						recPeopleDisplayURL="linkedin.com/in/holly-shackleton-28b27413a/"
@@ -141,12 +150,12 @@ const RecommendedPage = () => {
 					</RecommendedPeople>
 
 					<RecommendedPeople
-						recPeopleTitle="Chris Rutter"
-						recPeopleURL="https://www.linkedin.com/in/iamchrisrutter/"
-						recPeopleDisplayURL="linkedin.com/in/iamchrisrutter/"
+						recPeopleTitle="George Wood"
+						recPeopleURL="http://georgewood.me/"
+						recPeopleDisplayURL="georgewood.me"
 						recPeopleClasses = {"order-" +  + Math.floor(Math.random() * 10)}
-						recPeopleJob="Technical &amp; IT Guru"
-						recPeopleDescription="Chris's technical mind in how to integrate systems is high class - he works very hard in order to make things efficient and will make it his mission to understand a process first in order to improve it.">
+						recPeopleJob="UX/UI Designer &amp; Developer"
+						recPeopleDescription="I've watched George grow from a young Web Designer into a fully-fledged Senior UX/UI Designer, and some of the work he produces is just unreal. I envy his design ability.">
 					</RecommendedPeople>
 
 				</div>
