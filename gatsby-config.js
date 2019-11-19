b09d296de5055297dec563980f360a73a5fc9673
Dan Farrow <dan@zellement.com>
2019-11-19 12:59:28 +0000

    Adding Spotify

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,18 @@ module.exports = {
         component: require.resolve(`./src/pages/index.js`),
       },
     },*/
+    {
+      resolve: `gatsby-source-spotify`,
+      options: {
+        clientId: `b63eb9eb58984e24b561afba8cca15d6`,
+        clientSecret: `9341a11211634fed9dc4e8517753f09e`,
+        refreshToken: `AQBvUXU8z0boJNY3jzrqDGtbcnY0pwkOsPe8t6zWyo1tLWHvpj1oba4xv0wJ-9C5ot3FGPIkPwQcJV8DlnP6cNDsV6SsKHDRyRpCgNAm2jciDDjoRnygRixtStyfV_GCcwc`,
+    
+        fetchPlaylists: false, // optional. Set to false to disable fetching of your playlists
+        fetchRecent: true, // optional. Set to false to disable fetching of your recently played tracks
+        timeRanges: ['short_term', 'long_term'], // optional. Set time ranges to be fetched
+      },
+    },
     { 
       resolve: `gatsby-plugin-purgecss`,
       options: {
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,76 @@
 import React from "react"
+import { StaticQuery, graphql } from 'gatsby'
 import Logo from "./Logo"
 import Navigation from "./Navigation"
+import Img from 'gatsby-image';
 
-const Header = () => (
-	<header className="block fixed bg-brown-standard z-50 top-0 left-0 w-full md:w-1/3 lg:w-1/4">
-		<div className="container mx-auto">
-			<Logo />
-			<Navigation />
-		</div>
-	</header>
-)
+//npx gatsby-source-spotify token <clientId> <clientToken>
 
-export default Header
+export default  () => (
+	<StaticQuery
+	  query={graphql`
+	  query MyQuery {
+		allSpotifyTopTrack(sort: {fields: order, order: ASC}, limit: 3) {
+		  edges {
+			node {
+			  id
+			  name
+			  order
+			  album {
+				artists {
+				  name
+				}
+				name
+			  }
+			  image {
+				localFile {
+				  id
+				  childImageSharp {
+					fixed(width: 42) {
+						...GatsbyImageSharpFixed_withWebp
+					}
+				  }
+				}
+			  }
+			}
+		  }
+		}
+	  }
+	  
+	  `}
+	  
+	  render={data => (
+		<header className="block fixed bg-brown-standard z-50 top-0 left-0 w-full md:w-1/3 lg:w-1/4 md:h-full md:flex md:justify-between md:flex-col">
+			<div className="container mx-auto">
+				<Logo />
+				<Navigation />
+			</div>
+
+			<div className="hidden md:block text-right p-4 opacity-25 transition-fast hover:opacity-100">
+
+				<p className="uppercase text-white mb-4 text-md tracking-wider font-color-font">Recent Top Tracks</p>
+			{
+				data.allSpotifyTopTrack.edges.map(spotifyData => (
+				<div className="mt-2 flex justify-end" key={spotifyData.node.id}>
+					<div className="text-right my-auto pr-2">
+						<span className="uppercase text-white text-sm tracking-wider font-color-font">
+							{spotifyData.node.album.artists.map(albumData => (
+								<>
+								{albumData.name}
+								</>
+							))}
+						</span>
+						<span className="text-xs block uppercase tracking-wider font-color-font">
+							{spotifyData.node.name}
+						</span>
+					</div>
+					<Img className="block" fixed={spotifyData.node.image.localFile.childImageSharp.fixed} />
+				</div>
+				))
+			}
+			</div>
+
+		</header>
+	  )}
+	/>
+  )
